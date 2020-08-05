9af6fcafc04934afcbfeb32cd846c855d3d5ba2f
luke newton <lukeantonynewton@gmail.com>
2020-08-05 12:34:11 -0400

    hook up front end

--- app.js
+++ app.js
@@ -17,10 +17,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+//serve static files from build directory
+app.use(express.static(path.join(__dirname, 'build')));
+//serve index for any unknown paths
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,9 +48,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-//serve index for any unknown paths
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
